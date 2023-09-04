c7e8e4e1fde2bab11d7d7de19e54d7da4e197b34
rafa-el-cord <rafaelcordeiroh@gmail.com>
2023-09-04 02:23:50 -0300

    Actualización 03ya04SEP

---
+++ perfil.js
@@ -0,0 +1,25 @@
+const ulElement = document.getElementById("formJs");
+const formElement = document.getElementById("actualizarFormJs");
+
+function actualizarListaDeUsuarios(){
+    ulElement.innerHTML = "";
+    usuarios.forEach(function(usuario){
+        let li = document.createElement('li');
+        li.innerText = usuario.nombre + " " + usuario.apodo;
+        ulElement.appendChild(li);
+    });
+}
+
+formElement.addEventListener('submit', function(event){
+    event.preventDefault();
+    let nombreInput = formElement['nombre'].value;
+    let apellidoInput = formElement['apodo'].value;
+    let nuevoUsuario = {
+        nombre: nombreInput,
+        apodo: apodoInput
+    };
+    usuarios.push(nuevoUsuario);
+    actualizarListaDeUsuarios();
+});
+
+actualizarListaDeUsuarios();
