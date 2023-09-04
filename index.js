52ba2a07e1b1fa40cc29b1f92c2f8871dce30b93
rafa-el-cord <rafaelcordeiroh@gmail.com>
2023-09-04 18:12:45 -0300

    commit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const path =require("path");
 
 app.use(express.static(path.join(__dirname, "./client")));
 app.get("/", navigationController.getIndex);
+app.get("/favoritos", navigationController.getFavoritos);
+app.get("/concluidos", navigationController.getConcluidos);
+app.get("/encurso", navigationController.getEncurso);
+app.get("/perfil", navigationController.getPerfil);
 
 app.listen(8000,()=> {
     console.log("Servidor corriendo en el puerto 8000");
--- server/controllers/navigationController.js
+++ server/controllers/navigationController.js
@@ -5,4 +5,31 @@ const getIndex = (req, res) => {
     res.sendFile(url_path);
 }
 
-module.exports = { getIndex };
+const getFavoritos = (req, res) => {
+    const url_path = path.join(__dirname, "../../client/favoritos.html");
+    res.sendFile(url_path);
+}
+
+const getEncurso = (req, res) => {
+    const url_path = path.join(__dirname, "../../client/encurso.html");
+    res.sendFile(url_path);
+}
+
+const getConcluidos = (req, res) => {
+    const url_path = path.join(__dirname, "../../client/concluidos.html");
+    res.sendFile(url_path);
+}
+
+const getPerfil = (req, res) => {
+    const url_path = path.join(__dirname, "../../client/perfil.html");
+    res.sendFile(url_path);
+}
+
+
+module.exports = { 
+    getIndex,
+    getFavoritos,
+    getConcluidos,
+    getEncurso,
+    getPerfil
+};
